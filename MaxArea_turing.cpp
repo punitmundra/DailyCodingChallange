a4f0ee38739f6a944e8c0afa60c43fb3f4f78fdd
punitmundra <39169378+punitmundra@users.noreply.github.com>
2020-05-31 18:27:56 +0530

    problem statement

---
+++ MaxArea_turing.cpp
@@ -0,0 +1,22 @@
+/*
+You are the owner of a coworking space like WeWork and your office building is rectangular. 
+Your team just built wall partitions to create mini offices for startups. 
+This office campus is represented by a 2D array of 1s (floor spaces) and 0s (walls). 
+Each point on this array is a one foot by one foot square. 
+You need to calculate the number of offices. 
+A single office is bordered by walls and is constructed by placing floors next to each other, 
+horizontally and/or vertically. Two 1s adjacent to each other horizontally or vertically are always part of the same office.
+
+Function numOffices() has one parameter: grid - a 2D grid/array of 1s and 0s
+
+In this problem, our input format is as follows: The first line is the number of rows in the 2D array. The second line is the number of columns in the 2D array. The rest of the input contains the data to be processed.
+
+Here is an example of the raw input:
+
+4 
+5 
+11110 
+11010 
+11000 
+00000
+*/
